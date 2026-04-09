ceb26f45de1903cba8170ee9ea56349d4023f79d
阮鸿龙 <nguyenlongdev@proton.me>
2026-04-09 22:23:22 +0700

    Delete localstorage.user.js

--- localstorage.user.js
+++
@@ -1,9 +0,0 @@
-// ==UserScript==
-// @name         Thay Localstorage
-// @namespace    x
-// @version      1.0
-// @author       39
-// @match        https://www.ugphone.com/*
-// @grant        none
-// @require      https://raw.githubusercontent.com/longndev/Meo-Bypass/refs/heads/main/script.js
-// ==/UserScript==
